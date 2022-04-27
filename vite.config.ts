46cfc24b9cd238e2a06c92ea167ca36f4ea248d8
taka <tomo05061@gmail.com>
2022-04-27 22:22:07 +0900

    baseの修正

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig(({command}) => ({
   root: 'src',
-  base: command === 'build' ? 'barrage-generator' : './',
+  base: command === 'build' ? '/barrage-generator/' : './',
   build: {
     outDir: '../dist',
     emptyOutDir: true
