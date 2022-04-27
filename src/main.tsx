498aab0f2dfc9062f132a2fe74adc85e06494a9e
taka <tomo05061@gmail.com>
2022-04-27 23:17:53 +0900

    BASE＿URL追加

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
 <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Navigation />
       <Routes>
         {router.map((page, i) => (<Route key={`route_${i}`} {...page} />))}
